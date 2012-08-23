753163f032ee3b4dddc0a97f0059b7517d24a180
Xavier L <xavier.l@afrosoft.tk>
2012-08-23 05:01:11 -0400

    Updates MySQLDataSource documentation

--- lib/Magister/DataSource/MySQL.php
+++ lib/Magister/DataSource/MySQL.php
@@ -1,6 +1,19 @@
 <?php
 
+/**
+ * MySQLDataSource class.
+ * 
+ * Provides data from a MySQL database.
+ */
 class MySQLDataSource extends DataSource {
+    
+    /**
+     * Data source constructor.
+     * 
+     * Instanciates the PDO connection.
+     * 
+     * @param array $config
+     */
     public function __construct(array $config) {
         parent::__construct("mysql:host={$config['host']};dbname={$config['name']};port={$config['port']}", $config['user'], $config['pass']);
         $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
