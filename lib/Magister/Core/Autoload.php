9a95a77f9e8507a0d3a733bf58249c691f4778ee
Xavier L <xavier.l@afrosoft.tk>
2012-08-23 04:45:58 -0400

    Updates Autoload documentation

--- lib/Magister/Core/Autoload.php
+++ lib/Magister/Core/Autoload.php
@@ -1,39 +1,27 @@
 <?php
 
 /**
- * This file contains the Autoload functionality, and registers the loaders with 
- * SPL's hooks.
- * @package Magister 
- * @subpackage Core
- */
-spl_autoload_register(array('Autoload', 'loadException'));
-spl_autoload_register(array('Autoload', 'loadController'));
-spl_autoload_register(array('Autoload', 'loadModel'));
-spl_autoload_register(array('Autoload', 'loadHelper'));
-spl_autoload_register(array('Autoload', 'loadDataSource'));
-spl_autoload_register(array('Autoload', 'loadCore'));
-spl_autoload_register(array('Autoload', 'loadLib'));
-spl_autoload_register(array('Autoload', 'loadObject'));
-spl_autoload_register(array('Autoload', 'loadAppLib'));
-spl_autoload_register(array('Autoload', 'loadApp'));
-
-/**
+ * Autoload class.
+ * 
  * Handles the loading of the various classes and files required for the 
  * framework to function. The system makes use of SPL's class autoloading 
  * functionnality to load only the required files and classes as they are needed 
  * by the application. The functions can be called manually to force the loading 
  * of a file or class, or to load a file that contains no class.
+ * 
  * @package Magister
  * @subpackage Core
  */
 class Autoload {
 
     /**
-     * Searches for and loads a file in the application directory, returning 
-     * true if found and loaded, false otherwise.
+     * LoadApp method.
+     * 
+     * Searches for and loads a file in the application directory.
+     * 
      * @param string $name Name of the file or class.
      * @param string $ext Extension with a leading dot. Defaults to `.php`.
-     * @return bool 
+     * @return bool Status of the load.
      */
     public static function loadApp($name, $ext = '.php') {
         $file = APP_DIR . DS . $name . $ext;
@@ -45,19 +33,24 @@ class Autoload {
     }
 
     /**
-     * Loads a custom lib file in the app directory
+     * LoadAppLib method.
+     * 
+     * Loads a custom lib file in the application directory.
+     * 
      * @param string $name Name of the file or class.
-     * @return bool 
+     * @return bool Status of the load.
      */
     public static function loadAppLib($name) {
         return self::loadApp('Lib' . DS . $name);
     }
 
     /**
-     * Searches for and loads a controller by name, returning true if found and 
-     * loaded, false otherwise.
+     * LoadController method.
+     * 
+     * Searches for and loads a controller by name.
+     * 
      * @param string $name Name of the controller.
-     * @return bool 
+     * @return bool Status of the load.
      */
     public static function loadController($name) {
         if (strpos($name, 'Controller') !== false) {
@@ -67,10 +60,12 @@ class Autoload {
     }
 
     /**
-     * Searches for and loads a model by name, returning true if found and 
-     * loaded, false otherwise.
+     * LoadModel method.
+     * 
+     * Searches for and loads a model by name.
+     * 
      * @param string $name Name of the Model.
-     * @return bool 
+     * @return bool Status of the load.
      */
     public static function loadModel($name) {
         if (strpos($name, 'Model') !== false) {
@@ -80,10 +75,12 @@ class Autoload {
     }
 
     /**
-     * Searches for and loads an object by name, returning true if found and 
-     * loaded, false otherwise.
+     * LoadObject method.
+     * 
+     * Searches for and loads an object by name
+     * 
      * @param string $name Name of the object.
-     * @return bool 
+     * @return bool Status of the load.
      */
     public static function loadObject($name) {
         if (!class_exists('RowObject'))
@@ -92,11 +89,13 @@ class Autoload {
     }
 
     /**
-     * Searches for and loads a library file by name, returning true if found 
-     * and loaded, false otherwise.
+     * LoadLib method.
+     * 
+     * Searches for and loads a library file by name.
+     * 
      * @param string $name Name of the file or class.
      * @param string $ext Extension with a leading dot. Defaults to `.php`.
-     * @return bool 
+     * @return bool Status of the load.
      */
     public static function loadLib($name, $ext = '.php') {
         $common = LIB_DIR . DS . 'Magister' . DS . $name . $ext;
@@ -108,20 +107,24 @@ class Autoload {
     }
 
     /**
-     * Searches for and loads a core file by name, returning true if found and 
-     * loaded, false otherwise.
+     * LoadCore method.
+     * 
+     * Searches for and loads a core file by name.
+     * 
      * @param string $name Name of the file or class.
-     * @return bool 
+     * @return bool Status of the load.
      */
     public static function loadCore($name) {
         return self::loadLib('Core' . DS . $name);
     }
 
     /**
-     * Searches for and loads a helper by name, returning true if found and 
-     * loaded, false otherwise.
+     * LoadHelper method.
+     * 
+     * Searches for and loads a helper by name.
+     * 
      * @param string $name Name of the Helper.
-     * @return bool 
+     * @return bool Status of the load.
      */
     public static function loadHelper($name) {
         if (strpos($name, 'Helper') !== false) {
@@ -131,10 +134,12 @@ class Autoload {
     }
 
     /**
-     * Searches for and loads a datasource by name, returning true if found and 
-     * loaded, false otherwise.
+     * LoadDataSource method.
+     * 
+     * Searches for and loads a datasource by name.
+     * 
      * @param string $name Name of the datasource.
-     * @return bool 
+     * @return bool Status of the load.
      */
     public static function loadDataSource($name) {
         if (strpos($name, 'DataSource') !== false) {
@@ -144,9 +149,12 @@ class Autoload {
     }
 
     /**
+     * LoadException method.
+     * 
      * Loads all exceptions.
+     * 
      * @param string $name Name of the exception.
-     * @return bool 
+     * @return bool Status of the load.
      */
     public static function loadException($name) {
         if (strpos($name, 'Exception') !== false) {
@@ -156,3 +164,19 @@ class Autoload {
     }
 
 }
+
+/* The order can always be improved, but I currently set it so that it first 
+ * goes through the loaders that check for the name, no matter where their 
+ * location, then go through the application-specific loaders, than the library 
+ * loaders.
+ */
+spl_autoload_register(array('Autoload', 'loadException'));
+spl_autoload_register(array('Autoload', 'loadController'));
+spl_autoload_register(array('Autoload', 'loadModel'));
+spl_autoload_register(array('Autoload', 'loadHelper'));
+spl_autoload_register(array('Autoload', 'loadDataSource'));
+spl_autoload_register(array('Autoload', 'loadObject'));
+spl_autoload_register(array('Autoload', 'loadAppLib'));
+spl_autoload_register(array('Autoload', 'loadApp'));
+spl_autoload_register(array('Autoload', 'loadCore'));
+spl_autoload_register(array('Autoload', 'loadLib'));
